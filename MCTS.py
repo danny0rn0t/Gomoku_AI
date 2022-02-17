e44e54d7c675b9e481cc61574cf7a14a600abba7
danny0rn0t <b08902144@ntu.edu.tw>
2022-02-18 02:14:16 +0800

    fix mcts

--- MCTS.py
+++ MCTS.py
@@ -3,23 +3,25 @@ import numpy as np
 import math
 from game import gobang
 from policyGradientNetwork import *
+from copy import deepcopy
 class MCTS():
     def __init__(self, model: PolicyNetworkAgent):
         self.model = model
-        self.Qsa = {}
-        self.Nsa = {}
-        self.Ns = {}
-        self.Ps = {}
-        self.Es = {}
-        self.Vs = {}
+        self.Qsa = {} # expected reward for taking action a from state s
+        self.Nsa = {} # number of time taking action a from state s
+        self.Ns = {} # number of time state s was visited 
+        self.Ps = {} # policy output (probs) of neural network from state s
+        self.Es = {} # check game result at state s
+        self.Vs = {} # valid moves at state s
     
-    def simulateAndPredict(self, game: gobang, NUM_SIMULATION):
-        s = game.getBoard().tobytes()
+    def simulateAndPredict(self, startGame: gobang, NUM_SIMULATION):
+        s = startGame.getBoard().tobytes()
 
         for i in range(NUM_SIMULATION):
+            game = deepcopy(startGame)
             self.search(game)
         cnt = []
-        for a in range(game.boardsize * game.boardsize):
+        for a in range(startGame.boardsize * startGame.boardsize):
             if (s, a) not in self.Nsa:
                 cnt.append(0)
             else:
