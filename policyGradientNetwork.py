0650d92ce0abcd85c6aa8375bdfe4971dd99920b
danny0rn0t <b08902144@ntu.edu.tw>
2022-02-19 21:53:46 +0800

    bug fix

--- policyGradientNetwork.py
+++ policyGradientNetwork.py
@@ -100,12 +100,13 @@ class PolicyNetworkAgent():
                 y_pi = torch.FloatTensor(np.array(y_pi).astype(np.float64)).contiguous()
                 y_v = torch.FloatTensor(np.array(y_v).astype(np.float64)).contiguous()
                 print(f"debug: board.shape = {board.shape}, y_pi.shape = {y_pi.shape}, y_v.shape = {y_v.shape}")
-                
+
                 if self.args.cuda:
                     board = board.cuda()
                     y_pi = y_pi.cuda()
                     y_v = y_v.cuda()
                 X_pi, X_v = self.network(board)
+                print(f"debug: X_pi.shape = {X_pi.shape}, X_v.shape = {X_v.shape}")
                 loss = self.calcLoss(X_pi, y_pi, X_v, y_v)
                 self.optimizer.zero_grad()
                 loss.backward()
