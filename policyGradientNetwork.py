948cd93475613ab18b1973491f929c2f03f2687b
danny0rn0t <b08902144@ntu.edu.tw>
2022-02-21 01:11:33 +0800

    residual network

--- policyGradientNetwork.py
+++ policyGradientNetwork.py
@@ -124,13 +124,13 @@ class PolicyNetworkAgent():
         }
         torch.save(Agent_Dict, PATH)
     def load(self, PATH) -> int:
-        # try:
-        #     checkpoint = torch.load(PATH, strict=True)
-        #     print(f"Loading checkpoint from {PATH} ...")
-        # except:
-        #     print(f"Checkpoint not found, skip loading.")
-        #     return -1
-        checkpoint = torch.load(PATH)
+        try:
+            checkpoint = torch.load(PATH, strict=True)
+            print(f"Loading checkpoint from {PATH} ...")
+        except:
+            print(f"Checkpoint not found, skip loading.")
+            return -1
+        # checkpoint = torch.load(PATH)
         self.network.load_state_dict(checkpoint["network"])
         self.optimizer.load_state_dict(checkpoint["optimizer"])
         return 0
