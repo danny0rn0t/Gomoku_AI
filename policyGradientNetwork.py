284ae7462853ebac9e9c457234db53557778dec3
danny0rn0t <b08902144@ntu.edu.tw>
2022-03-12 16:25:07 +0800

    fix big bug

--- policyGradientNetwork.py
+++ policyGradientNetwork.py
@@ -88,7 +88,7 @@ class PolicyNetworkAgent():
         with torch.no_grad():
             pi, v = self.network(board)
         pi = pi.detach().cpu().numpy()[0]
-        v = v.detach().cpu().numpy().item()
+        v = v.detach().cpu().numpy()[0].item()
         return pi, v
     def learn(self, target):
         target = PolicyGradientNetworkDataset(target)
